7d72c84a0cd226a5fd8cad03ce681de5fc939660
Dima <dispikerton@gmail.com>
2021-07-14 12:44:57 +0300

    рекурсия, первая версия

--- src/main/java/ru/globaltruck/vaadin/NodeData.java
+++ src/main/java/ru/globaltruck/vaadin/NodeData.java
@@ -34,21 +34,23 @@ public class NodeData {
             Object map = stringObjectMap.get(keyParent);
             if (map instanceof HashMap) {
                 Map<String, Object> stringObjectMapLevel2 = (Map<String, Object>) map;
-                for (String keyLevel2 : stringObjectMapLevel2.keySet()){
-                    nodeList.add(new Node(keyLevel2, nodeList.get(indexParent)));
-                    int indexLevel2 = nodeList.size() - 1;
-                    Object mapLevel2 = stringObjectMapLevel2.get(keyLevel2);
-                    if (mapLevel2 instanceof HashMap) {
-                        Map<String, Object> stringObjectMap2 = (Map<String, Object>) mapLevel2;
-                        for (String keyLevel3 : stringObjectMap2.keySet()) {
-                            nodeList.add(new Node(keyLevel3, nodeList.get(indexLevel2)));
-                        }
-                    }
-                }
+                recursion(nodeList, indexParent, stringObjectMapLevel2);
             }
         }
 
         return nodeList;
+    }
+
+    private static void recursion(List<Node> nodeList, int indexParent, Map<String, Object> stringObjectMapLevel2) {
+        for (String keyLevel2 : stringObjectMapLevel2.keySet()){
+            nodeList.add(new Node(keyLevel2, nodeList.get(indexParent)));
+            int indexLevel2 = nodeList.size() - 1;
+            Object mapLevel2 = stringObjectMapLevel2.get(keyLevel2);
+            if (mapLevel2 instanceof HashMap) {
+                Map<String, Object> stringObjectMap2 = (Map<String, Object>) mapLevel2;
+                recursion(nodeList, indexLevel2, stringObjectMap2);
+            }
+        }
     }
 
     public List<Node> getRootDepartments() {
